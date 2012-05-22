e7366553e289e90725a9ee18e7f1426bcd3a268d
saramagliacane <sara.magliacane@gmail.com>
2012-05-22 19:36:12 +0200

    Adding the D2S logo and make the window undraggable.

--- src/com/data2semantics/hubble/client/HubbleInterface.java
+++ src/com/data2semantics/hubble/client/HubbleInterface.java
@@ -2,6 +2,7 @@ package com.data2semantics.hubble.client;
 import com.data2semantics.hubble.client.view.View;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.smartgwt.client.docs.Appearance;
 import com.smartgwt.client.widgets.Window;
 
 public class HubbleInterface implements EntryPoint
@@ -18,18 +19,20 @@ public class HubbleInterface implements EntryPoint
 			window.setAutoSize(true);
 			window.setHeight100();
 			window.setWidth100();
-			window.setMargin(0); //maybe change
 			
 			
-			window.setHeaderIcon("icons/fugue/navigation-090-white.png");
+			
+			window.setHeaderIcon("icons/logo-no-text-150dpi-small.png",20, 20);
 			window.setShowMaximizeButton(false);
 			window.setShowMinimizeButton(false);
 			window.setShowCloseButton(false);
+			window.setCanDrag(false);
+			window.setCanDragReposition(false);
+			//window.setHeaderSrc("icons/BlackBar.jpg"); // TODO: doesn't work, check if it's the size
+			//window.setMargin(10);
 			
 			View view = new View(serverSideApi);
 //			view.draw();
-						
-			//TODO: check
 			window.addChild(view);
 			window.draw();
 		}
