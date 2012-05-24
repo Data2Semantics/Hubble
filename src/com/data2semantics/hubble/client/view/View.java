708b31f1df3682d3d2add77023552f4e9e28c76e
saramagliacane <sara.magliacane@gmail.com>
2012-05-24 14:10:58 +0200

    Fixed size of bottom canvas

--- src/com/data2semantics/hubble/client/view/View.java
+++ src/com/data2semantics/hubble/client/view/View.java
@@ -50,7 +50,12 @@ public class View extends VLayout {
 		if (members.length > 1) {
 			removeMember(members[members.length-1]);
 		}
+//		canvas.setLeft(-20);
+		canvas.setWidth(PatientListing.WIDTH+ PatientInfo.RHS_WIDTH);
+		canvas.setHeight(PatientListing.HEIGHT);
+		
 		addMember(canvas);
+		
 	}
 	
 	public void onError( String error ){
